c638e88a6122c937a837fd2836514e9e691ebc4f
stdn08 <stdn08@COMP08>
2017-02-23 14:28:59 +0100

    openmp_cv2

--- openmp/openmp.cpp
+++ openmp/openmp.cpp
@@ -2,10 +2,93 @@
 //
 
 #include "stdafx.h"
-
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include <stdlib.h>
 
 int main()
 {
-    return 0;
+
+	std::vector<float> v = std::vector<float>();
+	std::chrono::time_point<std::chrono::system_clock> start, end;
+	double avg = 0.0;
+	int n = 1500000000;
+	int maxx = 1000;
+	int minn = 10;
+	for (int i = 0; i < n; i++) {
+		v.push_back((float)(rand() % maxx + minn));
+	}
+	
+	std::cout << "serial avg \n";
+	start = std::chrono::system_clock::now();
+
+	for (int i = 0; i < n; i++) {
+		avg += v[i];
+	}
+	end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << avg / n << " \n";
+	std::cout << elapsed_seconds.count() << " s\n";
+
+
+	std::cout << "parallel avg \n";
+	start = std::chrono::system_clock::now();
+	avg = 0.0;
+#pragma omp parallel for reduction(+:avg)
+	for (int i = 0; i < n; i++) {
+		avg += v[i];
+	}
+	
+	end = std::chrono::system_clock::now();
+	elapsed_seconds = end - start;
+	std::cout << avg / n << " \n";
+	std::cout << elapsed_seconds.count() << " s\n";
+
+	system("pause");
+
+	std::cout << "serial min \n";
+	double min = maxx+1;
+	start = std::chrono::system_clock::now();
+	for (int i = 0; i < n; i++) {
+		if(v[i]<min)min = v[i];
+	}
+	end = std::chrono::system_clock::now();
+	elapsed_seconds = end - start;
+	std::cout << min << " \n";
+	std::cout << elapsed_seconds.count() << " s\n";
+
+	std::cout << "parallel min \n";
+	start = std::chrono::system_clock::now();
+	min = maxx+1;
+#pragma omp parallel
+	{
+		double locmin = maxx+1;
+#pragma omp parallel for
+		for (int i = 0; i < n; i++) {
+			if (v[i] < locmin)locmin = v[i];
+		}
+#pragma omp critical
+		{
+			if (locmin < min)min = locmin;
+		}
+	}
+
+	end = std::chrono::system_clock::now();
+	elapsed_seconds = end - start;
+	std::cout << min << " \n";
+	std::cout << elapsed_seconds.count() << " s\n";
+
+	system("pause");
+
+
+
+
+
+
+
+	return 0;
 }
+
+
 
